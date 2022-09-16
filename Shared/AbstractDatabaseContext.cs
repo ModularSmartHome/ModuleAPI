23426633a9f3ea5d00832dc18c4c256d64a3edd7
Mathis <handymathis@gmail.com>
2022-09-16 23:19:04 +0200

    [TASK] Added rest client for fetching connection string

---
+++ Shared/AbstractDatabaseContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ModuleAPI.Models;
+
+namespace ModuleAPI.Shared;
+
+public class AbstractDatabaseContext : DbContext
+{
+    protected ArgumentListener Arguments;
+    
+    public AbstractDatabaseContext(ArgumentListener argumentListener)
+    {
+        Arguments = argumentListener;
+        Database.EnsureCreated();
+    }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var client = new RestClient(Arguments);
+        var task = client.GetConnectionKey();
+        var connectionString = task.GetAwaiter().GetResult();
+        Console.WriteLine("ConnectionString: " + connectionString);
+        optionsBuilder.UseNpgsql(connectionString);
+
+
+        base.OnConfiguring(optionsBuilder);
+    }
+
+}
---
+++ Shared/RestClient.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using System.Text.Json;
+using ModuleAPI.Models;
+
+namespace ModuleAPI.Shared;
+
+public class RestClient : HttpClient
+{
+    private ArgumentListener Args;
+
+    public RestClient(ArgumentListener args)
+    {
+        BaseAddress = new Uri(args.GetKernelHost());
+        Args = args;
+    }
+
+    public async Task<string> GetConnectionKey()
+    {
+        var key = Args.GetModuleKey();
+        var data = new
+        {
+            accessKey = key
+        };
+        var json = JsonSerializer.Serialize(data);
+        var stringContent = new StringContent(json, UnicodeEncoding.UTF8, "application/json");
+        HttpResponseMessage response = await PostAsync("/api/Module/GetDatabaseCredentials", stringContent);
+        return await response.Content.ReadAsStringAsync();
+    }
+
+}
