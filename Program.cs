9691c6ddeda2fc01fc828011c0ce5da3119471f4
Mathis <handymathis@gmail.com>
2022-09-17 00:01:21 +0200

    [TASK] Removed testing code

--- Program.cs
+++
@@ -1,9 +0,0 @@
-using ModuleAPI.Example;
-using ModuleAPI.Models;
-using ModuleAPI.Shared;
-
-var module = new ModuleAPI.ModuleAPI(args);
-module.SetActionHandler(new ExampleActionHandler())
-    .SetModuleInfo(new ModuleInfo("Test", "v1.0.0"))
-    .SetDatabaseContext<AbstractDatabaseContext>();
-module.RunServer();
